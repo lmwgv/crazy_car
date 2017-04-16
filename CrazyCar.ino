e385138d353162d530fdce218f73ff1c5e10fa68
Luis Muñiz Wesolowski <lmwgv@users.noreply.github.com>
2017-04-16 19:38:17 +0200

    Completed hit effect.
    Added random factor to cannon shot.

--- CrazyCar.ino
+++ CrazyCar.ino
@@ -28,6 +28,18 @@ const PROGMEM byte cannon_bitmap[]
     B11111111
 };
 
+const PROGMEM byte cup_bitmap[]
+{
+    6,7,
+    B11111100,
+    B01001000,
+    B01001000,
+    B01001000,
+    B00110000,
+    B00110000,
+    B01111000
+};
+
 const PROGMEM byte empty_heart_bitmap[]
 {
     7,6,
@@ -73,6 +85,8 @@ const byte gravity = 1;
 byte car_x = LCDWIDTH / 2;
 char car_y = LCDHEIGHT - 8;
 bool car_facing_left = true;
+const byte hit_cooldown = 40;
+byte car_hit_countdown = 0;
 char car_vy = 0;
 byte lives = 3;
 
@@ -128,6 +142,7 @@ void setup()
     gb.begin(); 
     gb.titleScreen(F("Crazy Car!!"), car_bitmap);
     gb.battery.show = false;
+    gb.pickRandomSeed();
     load_level(0);
 }
 
@@ -139,21 +154,21 @@ void loop()
         {
             time_remaining = fire_rate;
             bullet_fired = true;
-            bullet_x = cannon_x;
-            bullet_y = cannon_y;
+            bullet_x = (car_x > cannon_x + 4)? cannon_x + 8 : cannon_x;
+            bullet_y = cannon_y + 4;
             bullet_vy = 0;
             // Exact solution (computationally expensive):
             //bullet_vx = (car_x - cannon_x) * sqrt(gravity / (2.0 * (car_y - cannon_y)));
             // Approximate solution:
             byte y_factor;
-            char delta_y = car_y - cannon_y + 4;
+            char delta_y = car_y - bullet_y + 4;
             if(delta_y > 20)
                 y_factor = 2;
             else if(delta_y > 0)
                 y_factor = pgm_read_byte(cannon_fire_lut + delta_y);
             else
                 y_factor = 11;
-            bullet_vx = ((int)((car_x - cannon_x + 4) * y_factor)) >> 4;
+            bullet_vx = ((int)((car_x - bullet_x + random(-4, 12)) * y_factor)) >> 4;
         }
         if(gb.buttons.pressed(BTN_A))
         {
@@ -187,16 +202,19 @@ void loop()
             bullet_vy += gravity;
             if(bullet_y > LCDHEIGHT)
                 bullet_fired = false;
-            gb.display.drawFastHLine(bullet_x, bullet_y, 2);
+            gb.display.fillCircle(bullet_x, bullet_y, 2);
             if(car_x < bullet_x && bullet_x < car_x + 8 && car_y < bullet_y && bullet_y < car_y + 8)
             {
                 bullet_fired = false;
-                lives --;
+                lives--;
+                car_hit_countdown = hit_cooldown;
             }
         }
 
-        gb.display.drawBitmap(car_x, car_y, car_bitmap, NOROT, car_facing_left? NOFLIP:FLIPH);
-        gb.display.drawBitmap(cannon_x, cannon_y, cannon_bitmap, NOROT, car_x < cannon_x? NOFLIP:FLIPH);
+        if(!car_hit_countdown || car_hit_countdown-- & 0x2)
+            gb.display.drawBitmap(car_x, car_y, car_bitmap, NOROT, car_facing_left? NOFLIP:FLIPH);
+        gb.display.drawBitmap(cannon_x, cannon_y, cannon_bitmap, NOROT, car_x < cannon_x + 4? NOFLIP:FLIPH);
+        gb.display.drawBitmap(75, 1, cup_bitmap);
         for(int life=0; life<3; life++)
             gb.display.drawBitmap(life * 8, 0, (life < lives)? full_heart_bitmap: empty_heart_bitmap);
     }
